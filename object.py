c24ed48b177dce1ee2a19b76a0d963d57233e9c6
rushikeshwadghane <rushikeshwadghane35@gmail.com>
2022-06-28 14:59:07 +0530

    added object.py

---
+++ object.py
@@ -0,0 +1,15 @@
+class Dog:
+    breed = "shefard"
+    age = 23
+    colur = "white"
+    
+    
+    def fun(self):
+        print("This one is",self.breed)
+        print("This one is",self.colur)
+
+
+Rodger = Dog()
+
+print(Rodger.age)
+Rodger.fun()        
