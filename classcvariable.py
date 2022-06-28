23cb5c448cae93400cd7c4052d8b2b8ec81e63fd
rushikeshwadghane <rushikeshwadghane35@gmail.com>
2022-06-28 14:50:28 +0530

    added class variable.py

---
+++ classcvariable.py
@@ -0,0 +1,9 @@
+#class Variable and Instance variable 
+
+class Demo:
+    Language = "Python"  #class Variable 
+    
+    def __init__(self,lab,frame) -> None:
+        
+        self.lab = 'module'
+        self.frame = 'jango'
