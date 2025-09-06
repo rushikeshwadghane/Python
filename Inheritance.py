c90f6e943593aff97d07aae460bec91b8620f02a
rushikeshwadghane <rushikeshwadghane35@gmail.com>
2025-09-06 23:47:58 +0530

    new code

--- Inheritance.py
+++ Inheritance.py
@@ -1,8 +1,15 @@
 class Person:
-    def __init__(self,name):
+    def __init__(self, name):
         self.name = name
     
-    def getname(self):
-        return self.name 
-    
-       
+    def speak(self):
+        print(f"My name is {self.name}")
+
+class Student(Person):   # Student inherits Person
+    def study(self):
+        print(f"{self.name} is studying")
+
+# Using classes
+s = Student("Rushikesh")
+s.speak()   # inherited from Person
+s.study()   # own method
